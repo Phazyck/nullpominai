3276f0e89c5243fa845a9811d916ca3ae4b6f441
phazyck <phazyck@gmail.com>
2015-11-22 02:36:21 +0100

    Cleanup in Simulator.

--- src/dk/itu/ai/Simulator.java
+++ src/dk/itu/ai/Simulator.java
@@ -1,10 +1,12 @@
 package dk.itu.ai;
 
-import java.io.FileInputStream;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.newdawn.slick.util.Log;
 
 import mu.nu.nullpo.game.component.Controller;
+import mu.nu.nullpo.game.component.Piece;
 import mu.nu.nullpo.game.component.RuleOptions;
-import mu.nu.nullpo.game.event.EventReceiver;
 import mu.nu.nullpo.game.play.GameEngine;
 import mu.nu.nullpo.game.play.GameEngine.Status;
 import mu.nu.nullpo.game.play.GameManager;
@@ -13,100 +15,77 @@ import mu.nu.nullpo.game.subsystem.ai.DummyAI;
 import mu.nu.nullpo.game.subsystem.mode.GameMode;
 import mu.nu.nullpo.game.subsystem.mode.GradeMania3Mode;
 import mu.nu.nullpo.game.subsystem.wallkick.Wallkick;
-import mu.nu.nullpo.util.CustomProperties;
+import mu.nu.nullpo.gui.slick.LogSystemLog4j;
 import mu.nu.nullpo.util.GeneralUtil;
 import net.omegaboshi.nullpomino.game.subsystem.randomizer.Randomizer;
 
 public class Simulator {
-	
-	static String filename = "config\\rule\\Classic3.rul";
-	
+
 	public static void main(String[] args) {
 		
-		GameEngine gEngine = makeNewGame(
-				new GradeMania3Mode(), 
-				filename, 
-				new BasicAI());
+		PropertyConfigurator.configure("config/etc/log_slick.cfg");
+		Log.setLogSystem(new LogSystemLog4j());
 		
-		runGame(gEngine);
+		GameMode mode = 
+			new GradeMania3Mode();
 		
+		String rulePath = 
+			"config\\rule\\Classic3.rul";
+		
+		DummyAI ai = 
+			new BasicAI();
+		
+		Simulator simulator = new Simulator(mode, rulePath, ai);
+		
+		simulator.runSimulations(5);
 	}
 	
-	/**
-	 * Takes a game engine and runs it to completion (STATE == GAMEOVER)
-	 * @param gameEngine A freshly instantiated GameEngine object in the default beginning SETTING state.
-	 */
-	public static void runGame(GameEngine gameEngine) {
-		// Have to spend at least 5 frames in the menu before you can start the game
-		gameEngine.update();
-		gameEngine.update();
-		gameEngine.update();
-		gameEngine.update();
-		gameEngine.update();
-		gameEngine.ctrl.setButtonPressed(Controller.BUTTON_A); // Press A to start game
-		gameEngine.update(); 
-		gameEngine.ctrl.setButtonUnpressed(Controller.BUTTON_A);
-		
-		while (gameEngine.stat != Status.GAMEOVER) {
-			gameEngine.update();
-//			System.out.printf("State: %s\tLevel: %d\tCurrent Piece: %s  %n", 
-//					gameEngine.stat, 
-//					gameEngine.statistics.level, 
-//					(gameEngine.nowPieceObject != null ? gameEngine.nowPieceObject.toString() : "NONE"));
-		}
-		
-		System.out.println("Game is over!");
-		System.out.println("Final Level: " + gameEngine.statistics.level);
-	}
-	
-	
-	// Stolen from StateInGame:134
-	
-	/**
-	 * Make a new GameEngine object, ready to go
-	 * @param modeObj Game mode Object
-	 * @param strRulePath path string to the rules file
-	 * @param aiObj AI object to play (MAKE SURE IT SUPPORTS UNTHREADED !!! )
-	 * @return A freshly initiated GameEngine object, in the default SETTING state.
-	 */
-	public static GameEngine makeNewGame(GameMode modeObj, String strRulePath, DummyAI aiObj) {
-		GameManager gameManager = new GameManager(new EventReceiver());
+	private GameEngine engine;
 
-		gameManager.mode = modeObj;
+	/**
+	 * Make a new Simulator object, ready to go.
+	 * @param mode Game mode Object
+	 * @param rulePath path string to the rules file
+	 * @param ai AI object to play (MAKE SURE IT SUPPORTS UNTHREADED !!! )
+	 */
+	public Simulator(GameMode mode, String rulePath, DummyAI ai)
+	{
+		this(mode, GeneralUtil.loadRule(rulePath), ai);
+	}
+	
+	/**
+	 * Make a new Simulator object, ready to go.
+	 * @param mode Game mode Object
+	 * @param rules Game rules Object
+	 * @param ai AI object to play (MAKE SURE IT SUPPORTS UNTHREADED !!! )
+	 */
+	public Simulator(GameMode mode, RuleOptions rules, DummyAI ai)
+	{
+		/* 
+		 * NOTE(oliver): This code is a domain-specific version of 
+		 * mu.nu.nullpo.gui.slick.StateInGame.startNewGame(String strRulePath)
+		 */
+		GameManager gameManager = new GameManager();
+		
+		gameManager.mode = mode;
 		
 		gameManager.init();
 		
-		/* Shit stolen from GeneralUtil.java:265 */ 
-		CustomProperties prop = new CustomProperties();
-
-		try {
-			FileInputStream in = new FileInputStream(strRulePath);
-			prop.load(in);
-			in.close();
-		} catch (Exception e) {
-			System.out.println("Failed to load rule from " + strRulePath);
-		}
-
-		RuleOptions ruleopt = new RuleOptions();
-		ruleopt.readProperty(prop, 0);
-		/*----------------------------------------*/
-		
-		GameEngine engine = gameManager.engine[0]; 
-		
+		engine = gameManager.engine[0]; 
 		
 		// Rules
-		engine.ruleopt = ruleopt;
+		engine.ruleopt = rules;
 		
 		// Randomizer
-		Randomizer randomizerObject = GeneralUtil.loadRandomizer(ruleopt.strRandomizer);
+		Randomizer randomizerObject = GeneralUtil.loadRandomizer(rules.strRandomizer);
 		engine.randomizer = randomizerObject;
 
 		// Wallkick	
-		Wallkick wallkickObject = GeneralUtil.loadWallkick(ruleopt.strWallkick);
+		Wallkick wallkickObject = GeneralUtil.loadWallkick(rules.strWallkick);
 		engine.wallkick = wallkickObject;
 		
 		// AI
-		engine.ai = aiObj;
+		engine.ai = ai;
 		engine.aiMoveDelay = 0;
 		engine.aiThinkDelay = 0;
 		engine.aiUseThread = false;
@@ -114,12 +93,61 @@ public class Simulator {
 		engine.aiPrethink = false;
 		engine.aiShowState = false;
 		
-		
 		gameManager.showInput = false;
+	}
 
+	/**
+	 * Performs a single simulation to completion (STATE == GAMEOVER)
+	 */
+	public void runSimulation() 
+	{
 		// Called at initialization
 		engine.init();
+		
+		// Have to spend at least 5 frames in the menu before you can start the game
+		for(int i = 0; i < 5; ++i)
+		{
+			engine.update();
+		}
+		engine.ctrl.setButtonPressed(Controller.BUTTON_A); // Press A to start game
+		engine.update(); 
+		engine.ctrl.setButtonUnpressed(Controller.BUTTON_A);
+		
+		while (engine.stat != Status.GAMEOVER) {
+			engine.update();
+//			logGameState();
+		}
 
-		return engine;
+		log.info("Game is over!");
+		log.info("Final Level: " + engine.statistics.level);
+	}
+	
+	/**
+	 * Performs multiple sequential simulations to completion (STATE == GAMEOVER)
+	 */
+	public void runSimulations(int count) 
+	{
+		for(int i = 1; i <= count; ++i)
+		{
+			
+			log.info(String.format("-------- Simulation %d of %d --------", i, count));
+			runSimulation();
+		}
+	}
+
+	static Logger log = Logger.getLogger(Simulator.class);
+	
+	private void logGameState()
+	{
+		int level = engine.statistics.level;
+		
+		String piece = 
+			engine.nowPieceObject == null 
+				? " " 
+				: Piece.PIECE_NAMES[engine.nowPieceObject.id];
+		
+		String state = engine.stat.toString();
+		
+		log.info(String.format("\tLevel: %3d \tPiece: %s \tState: %s", level, piece, state));
 	}
 }
