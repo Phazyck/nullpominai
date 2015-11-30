df20ae85ed0057fd0f6b5b9b2f7182a822e0d1ca
SinaSohi <SinaSohi@gmail.com>
2015-11-30 06:02:33 +0100

    Added working Anji framework

---
+++ src/dk/itu/ai/TGMFitnessFunction.java
@@ -0,0 +1,32 @@
+package dk.itu.ai;
+
+import java.util.List;
+
+import org.jgap.BulkFitnessFunction;
+
+import com.anji.util.Configurable;
+import com.anji.util.Properties;
+
+public class TGMFitnessFunction implements BulkFitnessFunction, Configurable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init(Properties props) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void evaluate(List subjects) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getMaxFitnessValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
