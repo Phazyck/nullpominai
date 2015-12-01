7f1bf1c38b62a3dc1cff8ec127e49118db0964a6
phazyck <phazyck@gmail.com>
2015-12-01 20:23:57 +0100

    Refactored stage Debugger out of AI class.

--- src/dk/itu/ai/BasicAIProxy.java
+++ src/dk/itu/ai/BasicAIProxy.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import mu.nu.nullpo.game.component.Block;
 import mu.nu.nullpo.game.component.Controller;
 import mu.nu.nullpo.game.component.Field;
 import mu.nu.nullpo.game.component.Piece;
@@ -12,108 +11,7 @@ import mu.nu.nullpo.game.play.GameEngine;
 import mu.nu.nullpo.game.subsystem.ai.DummyAI;
 
 public class BasicAIProxy extends DummyAI {
-	
-	private int debugWidth = 14;
-	private int debugHeight = 24;
-	private int debugOffsetX = 2;
-	private int debugOffsetY = 2;
-	
-	private char[][] debugField = new char[debugHeight][debugWidth];
-	
-	private void resetDebugStage(int width, int height) 
-	{
-		for(int y = 0; y < debugHeight; ++y)
-		{
-			char[] line = debugField[y];
-			
-			for(int x = 0; x < debugWidth; ++x)
-			{
-				if((y < debugOffsetY)
-					|| (y >= debugOffsetY + height)
-					|| (x < debugOffsetX)
-					|| (x >= debugOffsetX + width))
-				{
-					line[x] = (char)9618; //'#';
-				}
-				else
-				{
-					line[x] = (char)9617;//' z';
-				}
-			
-				
-			}
-		}
-	}
-	
-	private void printDebugStage()
-	{
-		StringBuilder sb = new StringBuilder();
-		for(char[] debugLine : debugField)
-		{
-			sb.append('\n');
-			for(char block : debugLine)
-			{
-				sb.append(block);
-			}
-			
-		}
 		
-		System.out.println(sb.toString());
-		
-	}
-	
-	private void setDebugChar(char c, int x, int y)
-	{
-		debugField[debugOffsetY + y][debugOffsetX + x] = c;
-	}
-	
-	private void printStage(GameEngine engine, int pieceX, int pieceY, int pieceRt, char pieceChar)
-	{
-		Field field = engine.field;
-		int height = field.getHeight();
-		int width = field.getWidth();
-		resetDebugStage(width, height);
-			
-		for(int y = 0; y < height; ++y)
-		{
-			Block[] row = field.getRow(y);
-			
-			for(int x = 0; x < width; ++x)
-			{
-				int color = row[x].color;
-				
-				if(color != Block.BLOCK_COLOR_NONE)
-				{
-					setDebugChar((char)9632, x, y);
-				}
-			}
-		}
-		
-		Piece piece = engine.nowPieceObject;
-		
-		int[] dataX = piece.dataX[pieceRt];
-		int[] dataY = piece.dataY[pieceRt];
-		
-		if(dataX.length != dataY.length)
-		{
-			System.out.println("Length mismatch in piece data");
-		}
-		
-		for(int i = 0; i < dataX.length; ++i)
-		{
-			int px = pieceX + dataX[i];
-			int py = pieceY + dataY[i];
-			
-			setDebugChar(pieceChar, px, py);
-		}
-		
-		char d = (char)(pieceRt + (int)'0');
-		
-		setDebugChar(d, pieceX, pieceY);
-		
-		printDebugStage();
-	}
-	
 	@Override
 	public void setControl(GameEngine engine, int playerID, Controller ctrl) {
 		Piece piece = engine.nowPieceObject;
@@ -123,7 +21,7 @@ public class BasicAIProxy extends DummyAI {
 			int x = engine.nowPieceX;
 			int y = engine.nowPieceY;
 			int rt = piece.direction;
-			printStage(engine, x, y, rt, 'Y');
+			Debug.printStage(engine, x, y, rt, 'Y');
 			
 			
 			int input = 0;
@@ -133,7 +31,6 @@ public class BasicAIProxy extends DummyAI {
 			int nowRt = engine.nowPieceObject.direction;
 			
 			int diffX = lastPieceX - nowX;
-			int diffY = lastPieceY - nowY;
 			int diffRt = lastPieceRt - nowRt;
 			
 			todoRt -= diffRt;
@@ -191,20 +88,8 @@ public class BasicAIProxy extends DummyAI {
 			ctrl.setButtonBit(input);
 			
 			lastPieceX = nowX;
-			lastPieceY = nowY;
 			lastPieceRt = nowRt;
-
-//			String pieceName = Piece.getPieceName(piece.id);
-//			int offsetX = piece.dataOffsetX[rt];
-//			int offsetY = piece.dataOffsetY[rt];
-//			if(offsetX != 0 || offsetY != 0)
-//			{
-//				pieceName = "piece: " + pieceName;
-//			}
-			
 		}
-		
-		//super.setControl(engine, playerID, ctrl);
 	}
 	
 	private boolean userConfirmed()
@@ -236,8 +121,6 @@ public class BasicAIProxy extends DummyAI {
 	
 	private int lastPieceRt;
 	private int lastPieceX;
-	private int lastPieceY;
-	
 	
 	private int todoRt;
 	private int todoMove;
@@ -264,7 +147,7 @@ public class BasicAIProxy extends DummyAI {
 					fld.copy(engine.field);
 					int y = pieceNow.getBottom(x, nowY, rt, fld);
 					
-					printStage(engine, x, y, rt, (char)9633);
+					Debug.printStage(engine, x, y, rt, (char)9633);
 					
 					System.out.println("\nSuggested move:\n");
 					
@@ -322,7 +205,6 @@ public class BasicAIProxy extends DummyAI {
 						targetY = y;
 						
 						lastPieceX = engine.nowPieceX;
-						lastPieceY = engine.nowPieceY;
 						lastPieceRt = engine.nowPieceObject.direction;
 						
 						return;
---
+++ src/dk/itu/ai/Debug.java
@@ -0,0 +1,110 @@
+package dk.itu.ai;
+
+import mu.nu.nullpo.game.component.Block;
+import mu.nu.nullpo.game.component.Field;
+import mu.nu.nullpo.game.component.Piece;
+import mu.nu.nullpo.game.play.GameEngine;
+
+public class Debug 
+{
+	private static int debugWidth = 14;
+	private static int debugHeight = 24;
+	private static int debugOffsetX = 2;
+	private static int debugOffsetY = 2;
+	
+	private static char[][] debugField = new char[debugHeight][debugWidth];
+	
+	private static void resetDebugStage(int width, int height) 
+	{
+		for(int y = 0; y < debugHeight; ++y)
+		{
+			char[] line = debugField[y];
+			
+			for(int x = 0; x < debugWidth; ++x)
+			{
+				if((y < debugOffsetY)
+					|| (y >= debugOffsetY + height)
+					|| (x < debugOffsetX)
+					|| (x >= debugOffsetX + width))
+				{
+					line[x] = (char)9618; //'#';
+				}
+				else
+				{
+					line[x] = (char)9617;//' z';
+				}
+			
+				
+			}
+		}
+	}
+	
+	private static void printDebugStage()
+	{
+		StringBuilder sb = new StringBuilder();
+		for(char[] debugLine : debugField)
+		{
+			sb.append('\n');
+			for(char block : debugLine)
+			{
+				sb.append(block);
+			}
+			
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+	
+	private static void setDebugChar(char c, int x, int y)
+	{
+		debugField[debugOffsetY + y][debugOffsetX + x] = c;
+	}
+	
+	public static void printStage(GameEngine engine, int pieceX, int pieceY, int pieceRt, char pieceChar)
+	{
+		Field field = engine.field;
+		int height = field.getHeight();
+		int width = field.getWidth();
+		resetDebugStage(width, height);
+			
+		for(int y = 0; y < height; ++y)
+		{
+			Block[] row = field.getRow(y);
+			
+			for(int x = 0; x < width; ++x)
+			{
+				int color = row[x].color;
+				
+				if(color != Block.BLOCK_COLOR_NONE)
+				{
+					setDebugChar((char)9632, x, y);
+				}
+			}
+		}
+		
+		Piece piece = engine.nowPieceObject;
+		
+		int[] dataX = piece.dataX[pieceRt];
+		int[] dataY = piece.dataY[pieceRt];
+		
+		if(dataX.length != dataY.length)
+		{
+			System.out.println("Length mismatch in piece data");
+		}
+		
+		for(int i = 0; i < dataX.length; ++i)
+		{
+			int px = pieceX + dataX[i];
+			int py = pieceY + dataY[i];
+			
+			setDebugChar(pieceChar, px, py);
+		}
+		
+		char d = (char)(pieceRt + (int)'0');
+		
+		setDebugChar(d, pieceX, pieceY);
+		
+		printDebugStage();
+	}
+}
